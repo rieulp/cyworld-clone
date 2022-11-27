8b28bcacf4a1ea75a9b5fd043c4d83e939ec08f7
rayoungpark <rayoung1229@gmail.com>
2022-11-28 02:16:52 +0900

    feat: PlayList 컴포넌트 구현 + playlist 목업 데이터 추가

---
+++ components/Playlist.tsx
@@ -0,0 +1,122 @@
+import styled from "@emotion/styled";
+import React, { useCallback, useMemo, useState } from "react";
+import { Music, playlist } from "../mock/playlist";
+
+function Playlist() {
+  const [checkList, setCheckList] = useState(
+    Array.from({ length: playlist.length }, () => false)
+  );
+  const allChecked = useMemo(
+    () => checkList.every((check) => check),
+    [checkList]
+  );
+
+  const onChangeCheck = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target instanceof HTMLInputElement) {
+        const value = e.target.value;
+        if (value === "all") {
+          setCheckList(checkList.map(() => e.target.checked));
+        } else if (!isNaN(Number(value))) {
+          const newList = [...checkList];
+          newList[Number(value)] = !newList[Number(value)];
+          setCheckList(newList);
+        }
+      }
+    },
+    [checkList]
+  );
+
+  return (
+    <TableContainer>
+      <thead>
+        <tr>
+          <th>
+            <input
+              type="checkbox"
+              onChange={onChangeCheck}
+              checked={allChecked}
+              value="all"
+            />
+          </th>
+          <th>번호</th>
+          <th className="left">곡명</th>
+          <th className="left">아티스트</th>
+        </tr>
+      </thead>
+      <tbody>
+        {playlist.map((music, index) => (
+          <PlaylistItem
+            key={index}
+            index={index}
+            checked={checkList[index]}
+            onChange={onChangeCheck}
+            {...music}
+          />
+        ))}
+      </tbody>
+    </TableContainer>
+  );
+}
+
+interface PlaylistItemProps extends Music {
+  index: number;
+  checked: boolean;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+export function PlaylistItem({
+  index,
+  title,
+  artist,
+  checked,
+  onChange,
+}: PlaylistItemProps) {
+  return (
+    <tr>
+      <td>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={onChange}
+          value={index}
+        />
+      </td>
+      <td>{index + 1}</td>
+      <td className="left link">{title}</td>
+      <td className="left link">{artist}</td>
+    </tr>
+  );
+}
+
+export default Playlist;
+
+const TableContainer = styled.table`
+  width: 100%;
+  text-align: center;
+  font-size: 13px;
+  line-height: 23px;
+  .left {
+    text-align: left;
+  }
+  .link {
+    cursor: pointer;
+    color: ${({ theme }) => theme.colors.darkBlue};
+  }
+  input {
+    vertical-align: middle;
+  }
+  thead {
+    background: #eee;
+    border-top: 1px solid ${({ theme }) => theme.colors.grey_60};
+    border-bottom: 1px solid ${({ theme }) => theme.colors.grey_60};
+  }
+  th {
+    color: ${({ theme }) => theme.colors.grey_60};
+    font-weight: 700;
+  }
+  tbody {
+    tr {
+      border-bottom: 1px dotted ${({ theme }) => theme.colors.grey_60};
+    }
+  }
+`;
---
+++ mock/playlist.ts
@@ -0,0 +1,47 @@
+export interface Music {
+  title: string;
+  artist: string;
+}
+
+export const playlist: Music[] = [
+  {
+    title: "16 Shots",
+    artist: "Stefflon Don",
+  },
+  {
+    title: "낙하 (with 아이유)",
+    artist: "AKMU(악뮤)",
+  },
+  {
+    title: "무기력증",
+    artist: "이르다(irda)",
+  },
+  {
+    title: "I Feel Good",
+    artist: "Pink Sweat$",
+  },
+  {
+    title: "SOMEONE'S SOMEONE",
+    artist: "몬스타엑스",
+  },
+  {
+    title: "Weight In Gold",
+    artist: "Gallant",
+  },
+  {
+    title: "잊을만하면",
+    artist: "Crush",
+  },
+  {
+    title: "Vineyard (빈야드)",
+    artist: "우효(Oohyo)",
+  },
+  {
+    title: "7 Years",
+    artist: "Lukas Graham",
+  },
+  {
+    title: "2002",
+    artist: "Anne-Marie",
+  },
+];
